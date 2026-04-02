93519396c33d237b6e2ff1bd28e9ecebb51dfab1
SuyashDive2005 <dsapractice2425@gmail.com>
2026-04-02 22:20:56 +0530

    fix

--- Backend/src/middleware/auth.middleware.js
+++ Backend/src/middleware/auth.middleware.js
@@ -4,7 +4,7 @@ import { prisma } from "../lib/prisma.js";
 export const protectRoute = async (req, res, next) => {
   try {
     const token = req.cookies.jwt;
-
+    // console.log(token);
     if (!token) {
       return res
         .status(401)
